25fee2ea7aa9b76efef50fff113af57cb90ed5ef
Detmar Ruhfus <detmar.ruhfus@aogroup.co.za>
2024-09-19 08:57:20 +0200

    Fixed error in utils/rules

--- utils/rules.tsx
+++ utils/rules.tsx
@@ -3,6 +3,19 @@ import { useContext } from "react"
 import ProcessorContext from "store/processors/processor.context"
 import { Rule } from "store/processors/processor.interface"
 
+interface RuleProps {
+  type: any
+  hoveredRule: Rule
+  selectedRule: Rule
+  setSelectedRule: (rule: Rule | null) => void
+  hoveredTypes: Array<string>
+  selectedTypes: Array<string>
+  setSelectedTypes: (types: Array<string>) => void
+  setHoveredType: (type: any) => void
+  setHoveredRule: (type: any) => void
+  setHoverTypes: (data: any[]) => void
+}
+
 export const getRuleDescriptions = (result: string, rule_id: number) => {
   const procCtx = useContext(ProcessorContext)
   const desc: any = procCtx.rules.find((rule: Rule) => rule.id === rule_id)
